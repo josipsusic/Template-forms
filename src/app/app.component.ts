5cd8eb0f8deae957afa13b7fc216345a537c5c15
josipsusic <susic@uhp-digital.com>
2020-10-30 15:51:03 +0100

    Email validation example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // ngModel syncronises the value of the email input with email
   email: string;
+
+  onSubmit() {
+    console.log(this.email);
+  }
 }
